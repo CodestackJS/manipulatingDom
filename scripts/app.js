b5bd0a724704241b09f696604caf1f01e919b354
Jerie See Villanueva <codestackjs007@gmail.com>
2024-01-11 19:12:40 -0800

    manipulating the DOM Quiz

---
+++ scripts/app.js
@@ -0,0 +1,84 @@
+//Initialize variable
+let displayBtn1 = document.getElementById("displayBtn1");
+let displayBtn2 = document.getElementById("displayBtn2");
+let displayBtn3 = document.getElementById("displayBtn3");
+let displayBtn4 = document.getElementById("displayBtn4");
+let displayBtn5 = document.getElementById("displayBtn5");
+let displayBtn6 = document.getElementById("displayBtn6");
+let displayBtn7 = document.getElementById("displayBtn7");
+let displayBtn8 = document.getElementById("displayBtn8");
+let displayBtn9 = document.getElementById("displayBtn9");
+let displayBtn10 = document.getElementById("displayBtn10");
+let myString;
+let background = document.getElementById("background");
+
+
+//function for button1
+displayBtn1.addEventListener("click", function(){
+    alert("red");
+    displayArea.innerText = myString;
+    background.classList = "red";
+});
+
+//function for button2
+displayBtn2.addEventListener("click", function(){
+    alert("pink");
+    displayArea.innerText = myString;
+    background.classList = "pink";
+});
+
+//function for button3
+displayBtn3.addEventListener("click", function(){
+    alert("green");
+    displayArea.innerText = myString;
+    background.classList = "green";
+});
+
+//function for button4
+displayBtn4.addEventListener("click", function(){
+    alert("orange");
+    displayArea.innerText = myString;
+    background.classList = "orange";
+});
+
+//function for button5
+displayBtn5.addEventListener("click", function(){
+    alert("black");
+    displayArea.innerText = myString;
+    background.classList = "black";
+});
+//function for button6
+displayBtn6.addEventListener("click", function(){
+    alert("blue");
+    displayArea.innerText = myString;
+    background.classList = "blue";
+});
+
+//function for button7
+displayBtn7.addEventListener("click", function(){
+    alert("gray");
+    displayArea.innerText = myString;
+    background.classList = "gray";
+});
+
+
+//function for button8
+displayBtn8.addEventListener("click", function(){
+    alert("purple");
+    displayArea.innerText = myString;
+    background.classList = "purple";
+});
+
+//function for button9
+displayBtn9.addEventListener("click", function(){
+    alert("yellow");
+    displayArea.innerText = myString;
+    background.classList = "yellow";
+});
+
+//function for button10
+displayBtn10.addEventListener("click", function(){
+    alert("aqua");
+    displayArea.innerText = myString;
+    background.classList = "aqua";
+});
